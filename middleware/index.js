0107032ed376d902fef9e46f09ebdb8c7cb558d3
Nolan Picini <nolanpic@gmail.com>
2020-02-12 13:23:47 -0800

    Add user validation middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,9 @@
 const logger = require('./logger');
 const validateUserId = require('./validateUserId');
+const validateUser = require('./validateUser');
 
 module.exports = {
     logger,
-    validateUserId
+    validateUserId,
+    validateUser
 };
