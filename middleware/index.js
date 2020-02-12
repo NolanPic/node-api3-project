8e94275703631c2cf5fa95cf34fa3a5e5ce51c3b
Nolan Picini <nolanpic@gmail.com>
2020-02-12 13:29:40 -0800

    Add post validation middleware

--- middleware/index.js
+++ middleware/index.js
@@ -1,9 +1,11 @@
 const logger = require('./logger');
 const validateUserId = require('./validateUserId');
 const validateUser = require('./validateUser');
+const validatePost = require('./validatePost');
 
 module.exports = {
     logger,
     validateUserId,
-    validateUser
+    validateUser,
+    validatePost
 };
---
+++ middleware/validatePost.js
@@ -0,0 +1,13 @@
+const validatePost = (req, res, next) => {
+    if(!req.body) {
+        res.status(400).json({ message: "missing post data" });
+    }
+    else if(!req.body.text) {
+        res.status(400).json({ message: "missing required text field" });
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = validatePost;
